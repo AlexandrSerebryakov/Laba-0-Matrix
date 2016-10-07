b666e282346e1e5611979178002c7c35738d3e48
AlexandrSerebryakov <common190516@mail.ru>
2016-10-07 14:44:13 +0600

    Rejected commit: Default

--- src/main/java/MatrixData.java
+++ src/main/java/MatrixData.java
@@ -2,4 +2,21 @@
  * Created by user on 07.10.2016.
  */
 public class MatrixData {
+
+    private int rows;
+    private int columns;
+    private int[][] tab;
+
+    public MatrixData(int rows, int columns) {
+        this.rows=rows;
+        this.columns=columns;
+        matrixFill(rows, columns);
+            }
+    public void matrixFill(int m, int n){
+        tab=new int[m][n];
+        m=3;
+        n=3;
+    }
+
+
 }
