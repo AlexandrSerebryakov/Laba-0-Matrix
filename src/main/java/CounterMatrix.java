695c537cf08129aee8b090aa041cdcf595347bcd
AlexandrSerebryakov <common190516@mail.ru>
2016-10-08 13:12:04 +0600

    Rejected commit: Default

--- src/main/java/CounterMatrix.java
+++ src/main/java/CounterMatrix.java
@@ -2,4 +2,21 @@
  * Created by user on 07.10.2016.
  */
 public class CounterMatrix {
+    public static MatrixData multiplyMatrix(MatrixData matrix1, MatrixData matrix2){
+        int row3 = matrix1.getRow();
+        int col3 = matrix2.getCol();
+        int col1row2 = matrix1.getCol();
+
+        MatrixData result = new MatrixData(row3, col3);
+
+        for (int i=0; i < row3; i++){
+            for (int j=0; j < col3; j++){
+                int values = 0;
+                for (int k = 0; k < col1row2; k++){
+
+                }
+
+            }
+        }
+    }
 }
