46687114bedd6887bca736bde6491e8d279fb287
AlexandrSerebryakov <common190516@mail.ru>
2016-10-09 13:24:36 +0600

    Rejected commit: Default

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,6 +3,12 @@
  */
 public class Runner {
     public static void main(String[] args) {
+       MatrixData matrix1 = new MatrixData(2, 2);
+        MatrixData matrix2 = new MatrixData(2, 2);
 
+        matrix1.fillRandom(2, 2);
+        matrix2.fillRandom(2, 2);
+
+        matrix3 = CounterMatrix.multiplyMatrix(matrix1, matrix2);
     }
 }
