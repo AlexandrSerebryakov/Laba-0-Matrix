6ba33738ada3f6de25f01feffa877f0597cce279
AlexandrSerebryakov <common190516@mail.ru>
2016-10-09 19:05:30 +0600

    Rejected commit: Default

--- src/main/java/OutputMatrix.java
+++ src/main/java/OutputMatrix.java
@@ -2,14 +2,14 @@
  * Created by user on 07.10.2016.
  */
 public class OutputMatrix {
-    //private MatrixData m1 = Runner.mai;
-    //private MatrixData m2;
-    //private MatrixData m3;
+    private MatrixData m1;
+    private MatrixData m2;
+    private MatrixData m3;
 
     public void print(){
         //MatrixData matrix1 = Runner.main().matrix1;
-        System.out.println(matrix1);
-        System.out.println(matrix2);
-        System.out.println(matrix3);
+        System.out.println(m1);
+        System.out.println(m2);
+        System.out.println(m3);
     }
 }
