657700ce4e198731a4f874aead6bc5f173ada3c1
AlexandrSerebryakov <common190516@mail.ru>
2016-10-11 23:48:09 +0600

    Rejected commit: Default

--- src/main/java/com/epam/alex/MatrixMultiplication.java
+++ src/main/java/com/epam/alex/MatrixMultiplication.java
@@ -9,7 +9,7 @@ public class MatrixMultiplication {
 
     public    static Matrix multiply(Matrix matrix1, Matrix matrix2) {
 
-        int matrix3[][] = new int[matrix1.getRow()][matrix2.getCol()];
+        int[][] matrix3 = new int[matrix1.getRow()][matrix2.getCol()];
         for (int i = 0; i < matrix1.getRow(); i++) {
             for (int j = 0; j < matrix2.getCol(); j++) {
 
@@ -17,7 +17,7 @@ public class MatrixMultiplication {
                     matrix3[i][j] += matrix1.getValue(i, k) * matrix2.getValue(k, j);
             }
         }
-        return new Matrix (matrix3);
+        return new Matrix(matrix3);
     }
 
 }
