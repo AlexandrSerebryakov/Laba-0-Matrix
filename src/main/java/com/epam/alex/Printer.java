ff94cb6f600a37bf42420685d2ce707d325c08b9
AlexandrSerebryakov <common190516@mail.ru>
2016-10-12 01:57:43 +0600

    Rejected commit: Default

--- src/main/java/com/epam/alex/Printer.java
+++ src/main/java/com/epam/alex/Printer.java
@@ -5,13 +5,17 @@ package com.epam.alex;
  */
 public class Printer {
     public static void print(Matrix matrix) {
+        //int i1;
+        //int j1;
         int a = matrix.getRow();
         int b = matrix.getCol();
+        //int c = matrix.getValue(int i,int j);
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < b; b++) {
-                System.out.println(matrix.getValue(i, j));
+                System.out.println(matrix.getValue(i,j));
             }
         }
+
         //int a = getRow();
         //}
 
