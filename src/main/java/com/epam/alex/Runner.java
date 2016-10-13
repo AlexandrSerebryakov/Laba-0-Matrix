990aae5d9dab4c93f4fcac532dc5b168151c9614
AlexandrSerebryakov <common190516@mail.ru>
2016-10-14 01:08:11 +0600

    Add annotation  to class "Main"

--- src/main/java/com/epam/alex/Runner.java
+++ src/main/java/com/epam/alex/Runner.java
@@ -13,15 +13,14 @@ package com.epam.alex;
 
 public class Runner {
     public static void main(String[] args) {
-
+        //Create and fill matrices
         Matrix matrixA = new Matrix(3, 3);
         Matrix matrixB = new Matrix(3, 3);
-
         matrixA.fillRandom(50);
         matrixB.fillRandom(50);
 
         int result[][] = MatrixMultiplier.multiply(matrixA, matrixB);
-
+        //Multiply matrices
         MatrixPrinter.printMatrixAB(matrixA);
         MatrixPrinter.printMatrixAB(matrixB);
         MatrixPrinter.printResult(result, matrixA, matrixB);
